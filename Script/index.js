91f5bb39847e0793c22d2e8c8804f49c41ca861a
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-11-15 00:21:38 +0100

    Create index.js

---
+++ Script/index.js
@@ -0,0 +1,20 @@
+
+//3
+var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"]
+
+//4
+console.log(planets[0])
+console.log(planets[1])
+console.log(planets[2])
+console.log(planets[3])
+console.log(planets[4])
+
+
+//7
+
+var hello = function (){
+    return 'Hello World!'
+}
+//8
+console.log(hello())
+
